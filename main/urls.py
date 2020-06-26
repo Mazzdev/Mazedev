ad2d394f409ddd249f2e57dd5fb68fb60d7445d0
mazedev <duldominik16@gmail.com>
2020-06-26 16:23:52 +0200

    contact form

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, portfolio, contact
+from .views import home, portfolio, contact, success2
 
 urlpatterns = [
     path('', home),
     path('portfolio', portfolio),
     path('contact', contact),
+    path('success2', success2),
 ]
--- main/views.py
+++ main/views.py
@@ -1,12 +1,36 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http import HttpResponse, BadHeaderError
+from django.shortcuts import render, redirect
+
+from .forms import ContactForm
+
 
 # Create your views here.
 
 def home(request):
     return render(request, 'main/about.html', {'title': 'about'})
 
+
 def portfolio(request):
     return render(request, 'main/portfolio.html', {'title': 'portfolio'})
 
+
+def success2(request):
+    return render(request, 'main/success2.html', {'title': 'Thanks for your message'})
+
+
 def contact(request):
-    return render(request, 'main/contact.html', {'title': 'contact'})
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ['admin@example.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return render(request, 'main/success2.html', {'title': 'Thanks for your message'})
+    return render(request, "main/contact.html", {'form': form})
